fb9782fb169d19a8295802270decb53da150c788
luowenjun@inke.cn <luowenjun@inke.cn>
2024-04-16 19:11:06 +0800

    chore: try decode

--- transaction/subscriber.go
+++ transaction/subscriber.go
@@ -6,15 +6,18 @@ import "context"
 type Subscriber interface {
 }
 
-// AddressChecker 地址条件检查
-type AddressChecker interface {
-	Support(ctx context.Context, fromAddress, toAddress string) bool
+// Checker 检查是否支持处理
+type Checker interface {
+	// SupportAddress 是否处理该地址
+	SupportAddress(ctx context.Context, fromAddress, toAddress string) bool
+
+	// SupportTransaction 是否处理该交易
+	SupportTransaction(ctx context.Context, txID string) bool
 }
 
 type FilterCond struct {
 	Type        string   // 交易类型
 	ToAddresses []string // 目标账户
-
 }
 
 type Filter interface {
